d4b1275212c1ea214478b7c591ba1695ed072b99
EvangelistaL <lucas.silva@sptech.school>
2024-07-02 20:14:40 -0300

    Adding Async config and making DefaultSendEmail async

--- src/main/java/com/kael/internal/usecase/DefaultSendEmail.java
+++ src/main/java/com/kael/internal/usecase/DefaultSendEmail.java
@@ -9,12 +9,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.util.Objects;
 import java.util.Set;
 
+@Async
 @Service
 @Slf4j
 @RequiredArgsConstructor
